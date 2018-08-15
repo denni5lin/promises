bd8c31822d923b107b3d023a73c12bbda7b22cb9
Dennis Lin <lin.dennis18@gmail.com>
2018-08-15 08:29:05 -0700

    Wordnik request URL update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
-let wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=-1&api_key=69831269a789de393b00907a0250ea3d3203df34417d4d5a3"
+let wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?&minLength=3&maxLength=-1&api_key=69831269a789de393b00907a0250ea3d3203df34417d4d5a3"
 let giphyAPI = "http://api.giphy.com/v1/gifs/search?q=" 
 let giphyKey = "&api_key=RI99LR6sMfDO6TXz4lSpeBSNryxXyI0x&limit=5"
-
-
 
 function setup() {
 
